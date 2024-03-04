04778b0877da11ce0a4febaa372af0d068b540fe
Dean Hulkenberg <Dean.Hulkenberg@edu.sait.ca>
2024-03-04 08:59:21 -0700

    added 4 articals

--- app/components/newsTab.js
+++ app/components/newsTab.js
@@ -15,8 +15,26 @@ const NewsTab = () => {
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
                             <div className="flex-shrink-0">
-                    <h1>hello</h1>
-                        </div>
+                                <a href="/">
+                                    News artical 1
+                                </a>
+                            </div>
+                            
+                    </div>
+                    <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <h1>News artical 2</h1>
+                            </div>
+                    </div>
+                    <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <h1>News artical 3</h1>
+                            </div>
+                    </div>
+                    <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <h1>News artical 4</h1>
+                            </div>
                     </div>
                 </div>
             </div>
