c44d69cd73f4ac1f1f8dacbc1f7458cd31e67e5e
Raesley <r.d.hempsall@gmail.com>
2024-03-03 20:39:59 -0700

    Configuring react routing
    Not complete - work to date.

--- app/messageboard/Home.js
+++ app/messageboard/Home.js
@@ -1,17 +1,44 @@
-import {React} from 'react'
+import {React, useState, useEffect} from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import './HomeStyles.css';
 
 function Home() {
+
+    const [input, setInput] = useState('');
+    const [channelNames, setChannelNames] = useState(['test1', 'test2']);
+
+    const addChannel = () => {
+        setChannelNames([...channelNames]);
+        setInput('');
+    }
+
     return (
         <center>
-        <div class='main'>
+        <div class='mainDiv'>
         <h1>Climate Chat</h1>
         <br />
+        
         <div class='addChannel'>
-            <input class='channelInput' placeholder='Enter the name of the channel.' />
-            <button>Add Channel</button>
+            <input value ={input} onChange={event => setInput(event.target.value)} class='channelInput' placeholder= 'Enter the name'/>
+            <button onClick = {addChannel}>Add Channel</button>
             <hr />
             <div class='channelList'>
-            <h5>Test</h5>
+        {/* {channelNames.map(channelName =>(<h5>{channelName}</h5>))} */
+
+        //Figuring out how react routing works.
+        <Router>
+            <ul>
+                <li>
+                    {"/channelName"}
+                </li>
+            </ul>
+        </Router>
+        }
             </div>
          </div>
         </div>
