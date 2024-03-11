0eba1068fab2f31cfbd7988a846a471ca533e6d0
Jhio0 <sorianojhio218@gmail.com>
2024-03-11 15:12:14 -0600

    Deployment6.2

--- app/reports/page.js
+++ app/reports/page.js
@@ -58,6 +58,7 @@ export default function ReportPage() {
         if (typeof window !== 'undefined') {
             loadReports();
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [user]);
 
     
