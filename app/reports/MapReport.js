f60a70bda48b54a19242c327b8636a9650a0d8a6
Jhio0 <sorianojhio218@gmail.com>
2024-03-11 17:55:34 -0600

    Deploymen#7

--- app/reports/MapReport.js
+++ app/reports/MapReport.js
@@ -1,14 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import React, { useEffect, useState, useRef } from 'react';
-
 import { MapContainer, TileLayer, GeoJSON, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import 'leaflet-geosearch/dist/geosearch.css';
-
 import { countryCodeToName, countryCodes } from '../components/items/countryUtils';
-
 
 export default function MapReport() {
   const [geojsonFeatures, setGeojsonFeatures] = useState([]);
@@ -21,94 +18,96 @@ export default function MapReport() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const provider = new OpenStreetMapProvider();
-        // Fetch emissions data for North American countries
-        const responseNorthAmerica = await fetch('https://api.climatetrace.org/v4/assets?continent=NA');
-        const { assets: assetsNorthAmerica } = await responseNorthAmerica.json();
+        if (typeof window !== 'undefined') {
+          const provider = new OpenStreetMapProvider();
+          // Fetch emissions data for North American countries
+          const responseNorthAmerica = await fetch('https://api.climatetrace.org/v4/assets?continent=NA');
+          const { assets: assetsNorthAmerica } = await responseNorthAmerica.json();
   
-        // Define a function to fetch emissions data for a single country with a delay
-        const fetchEmissionsDataWithDelay = async (countryCode) => {
-          const response = await fetch(`https://api.climatetrace.org/v4/country/emissions?&countries=${countryCode}`);
-          const emissionsData = await response.json();
-          
-          // Convert country code to full country name
-          const countryName = countryCodeToName(countryCode);
-  
-          const searchResult = await provider.search({ query: countryName });
-          let coordinates = [0, 0]; // Default coordinates
-          if (searchResult.length > 0) {
-            coordinates = [searchResult[0].x, searchResult[0].y]; // Extracting longitude and latitude
+          // Define a function to fetch emissions data for a single country with a delay
+          const fetchEmissionsDataWithDelay = async (countryCode) => {
+            const response = await fetch(`https://api.climatetrace.org/v4/country/emissions?&countries=${countryCode}`);
+            const emissionsData = await response.json();
+            
+            // Convert country code to full country name
+            const countryName = countryCodeToName(countryCode);
+    
+            const searchResult = await provider.search({ query: countryName });
+            let coordinates = [0, 0]; // Default coordinates
+            if (searchResult.length > 0) {
+              coordinates = [searchResult[0].x, searchResult[0].y]; // Extracting longitude and latitude
+            }
+    
+            return { countryCode, countryName, coordinates, emissionsData };
+          };
+    
+          // Fetch carbon emissions data for each North American country with a delay
+          const emissionsResults = [];
+          for (const countryCode of countryCodes) {
+            const result = await fetchEmissionsDataWithDelay(countryCode);
+            emissionsResults.push(result);
+            // Introduce a delay of 1 second before making the next request
+            await new Promise(resolve => setTimeout(resolve, 1000));
           }
-  
-          return { countryCode, countryName, coordinates, emissionsData };
-        };
-  
-        // Fetch carbon emissions data for each North American country with a delay
-        const emissionsResults = [];
-        for (const countryCode of countryCodes) {
-          const result = await fetchEmissionsDataWithDelay(countryCode);
-          emissionsResults.push(result);
-          // Introduce a delay of 1 second before making the next request
-          await new Promise(resolve => setTimeout(resolve, 1000));
-        }
-  
-        // Create GeoJSON features for emissions data
-        const emissionsFeatures = assetsNorthAmerica.map(source => {
-          const { Emissions } = source;
-          let co2_2022 = 'N/A';
-          if (Emissions) {
-            for (let i = 0; i < Emissions.length; i++) {
-              if (Emissions[i]['2022']) {
-                co2_2022 = Emissions[i]['2022'].find(emission => emission.co2)?.co2 || 'N/A';
-                break;
+    
+          // Create GeoJSON features for emissions data
+          const emissionsFeatures = assetsNorthAmerica.map(source => {
+            const { Emissions } = source;
+            let co2_2022 = 'N/A';
+            if (Emissions) {
+              for (let i = 0; i < Emissions.length; i++) {
+                if (Emissions[i]['2022']) {
+                  co2_2022 = Emissions[i]['2022'].find(emission => emission.co2)?.co2 || 'N/A';
+                  break;
+                }
               }
             }
-          }
-  
-          return {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [source.Centroid.Geometry[0], source.Centroid.Geometry[1]]
-            },
-            properties: {
-              name: source.Name,
-              province: source.Country,
-              co2_2022,
-              owners: source.Owners
-            }
-          };
-        });
-  
-        // Create GeoJSON features for carbon emissions data
-        const carbonEmissionFeatures = emissionsResults.map(result => {
-          const { countryCode, countryName, coordinates, emissionsData } = result;
-          if (emissionsData.length > 0) {
-            const { emissions } = emissionsData[0];
-            const co2Emissions = emissions?.co2 || 'N/A'; // Extract CO2 emissions
+    
             return {
               type: "Feature",
               geometry: {
                 type: "Point",
-                coordinates: coordinates // Use longitude and latitude
+                coordinates: [source.Centroid.Geometry[0], source.Centroid.Geometry[1]]
               },
               properties: {
-                countryCode: countryCode,
-                countryName: countryName,
-                co2Emissions: co2Emissions // Assign CO2 emissions
+                name: source.Name,
+                province: source.Country,
+                co2_2022,
+                owners: source.Owners
               }
             };
-          } else {
-            return null;
-          }
-        }).filter(feature => feature !== null);
-  
-        // Merge features from both emissions data and carbon emissions data
-        const mergedFeatures = [...emissionsFeatures];
-  
-        setGeojsonFeatures(mergedFeatures);
-        setGeojsonFeaturesCountry(carbonEmissionFeatures);
-        setLoading(false);
+          });
+    
+          // Create GeoJSON features for carbon emissions data
+          const carbonEmissionFeatures = emissionsResults.map(result => {
+            const { countryCode, countryName, coordinates, emissionsData } = result;
+            if (emissionsData.length > 0) {
+              const { emissions } = emissionsData[0];
+              const co2Emissions = emissions?.co2 || 'N/A'; // Extract CO2 emissions
+              return {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: coordinates // Use longitude and latitude
+                },
+                properties: {
+                  countryCode: countryCode,
+                  countryName: countryName,
+                  co2Emissions: co2Emissions // Assign CO2 emissions
+                }
+              };
+            } else {
+              return null;
+            }
+          }).filter(feature => feature !== null);
+    
+          // Merge features from both emissions data and carbon emissions data
+          const mergedFeatures = [...emissionsFeatures];
+    
+          setGeojsonFeatures(mergedFeatures);
+          setGeojsonFeaturesCountry(carbonEmissionFeatures);
+          setLoading(false);
+        }
       } catch (error) {
         console.error('Error fetching emissions data:', error);
         setLoading(false);
@@ -122,59 +121,61 @@ export default function MapReport() {
   
   const handleFeatureClick = async (feature) => {
     try {
-      // Extracting coordinates of the clicked feature
-      const { coordinates } = feature.geometry;
-      const [longitude, latitude] = coordinates;
-  
-      // Fetching emissions data for the clicked coordinates with a delay
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      const response = await fetch(`https://api.climatetrace.org/v4/assets?latitude=${latitude}&longitude=${longitude}`);
-      const { assets } = await response.json();
-  
-      // Find the closest asset to the clicked coordinates
-      const selectedAsset = findClosestAsset(assets, longitude, latitude);
-  
-      // Extracting CO2 and CH4 emissions for the year 2022
-      const emissions2022 = selectedAsset.Emissions?.find(emission => emission["2022"]);
-      const co2Emissions = emissions2022?.["2022"]?.find(emission => emission.co2)?.co2 || 'N/A';
-      const ch4Emissions = emissions2022?.["2022"]?.find(emission => emission.ch4)?.ch4 || 'N/A';
-  
-      // Update selectedData state with the fetched emissions data for the selected asset
-      setSelectedData([
-        { label: `${selectedAsset.name} - CO2 Emissions (Tons)`, value: co2Emissions },
-        { label: `${selectedAsset.name} - CH4 Emissions (Tons)`, value: ch4Emissions }
-      ]);
-  
-      // Update ownerEmissions state with the percentage emissions for each owner
-      if (selectedAsset.Owners && selectedAsset.Owners.length > 0) {
-        const totalEmissions = parseFloat(co2Emissions) + parseFloat(ch4Emissions);
-        const ownerPercentageEmissions = selectedAsset.Owners.map(owner => ({
-          label: `${owner.CompanyName} - Percentage Emissions`,
-          value: ((parseFloat(owner.PercentageOfInterestCompany) / 100) * totalEmissions).toFixed(2)
-        }));
-        setOwnerEmissions(ownerPercentageEmissions);
-      } else {
-        // If no owners, set ownerEmissions to an empty array
-        setOwnerEmissions([]);
-      }
-  
-      // If the clicked feature is a country, fetch emissions data for that country and update selectedData
-      if (feature.properties.countryCode && feature.properties.countryName) {
-        const { countryCode, countryName } = feature.properties;
-  
-        // Fetch emissions data for the clicked country with a delay
+      if (typeof window !== 'undefined') {
+        // Extracting coordinates of the clicked feature
+        const { coordinates } = feature.geometry;
+        const [longitude, latitude] = coordinates;
+    
+        // Fetching emissions data for the clicked coordinates with a delay
         await new Promise(resolve => setTimeout(resolve, 1000));
-        const countryResponse = await fetch(`https://api.climatetrace.org/v4/country/emissions?&countries=${countryCode}`);
-        const countryEmissionsData = await countryResponse.json();
-  
-        // Extract CO2 emissions for the clicked country
-        const countryCo2Emissions = countryEmissionsData.length > 0 ? countryEmissionsData[0]?.emissions?.co2 || 'N/A' : 'N/A';
-  
-        // Update selectedData state with the fetched CO2 emissions data for the selected country
+        const response = await fetch(`https://api.climatetrace.org/v4/assets?latitude=${latitude}&longitude=${longitude}`);
+        const { assets } = await response.json();
+    
+        // Find the closest asset to the clicked coordinates
+        const selectedAsset = findClosestAsset(assets, longitude, latitude);
+    
+        // Extracting CO2 and CH4 emissions for the year 2022
+        const emissions2022 = selectedAsset.Emissions?.find(emission => emission["2022"]);
+        const co2Emissions = emissions2022?.["2022"]?.find(emission => emission.co2)?.co2 || 'N/A';
+        const ch4Emissions = emissions2022?.["2022"]?.find(emission => emission.ch4)?.ch4 || 'N/A';
+    
+        // Update selectedData state with the fetched emissions data for the selected asset
         setSelectedData([
-          ...selectedData,
-          { label: `${countryName} - CO2 Emissions (Tons)`, value: countryCo2Emissions }
+          { label: `${selectedAsset.name} - CO2 Emissions (Tons)`, value: co2Emissions },
+          { label: `${selectedAsset.name} - CH4 Emissions (Tons)`, value: ch4Emissions }
         ]);
+    
+        // Update ownerEmissions state with the percentage emissions for each owner
+        if (selectedAsset.Owners && selectedAsset.Owners.length > 0) {
+          const totalEmissions = parseFloat(co2Emissions) + parseFloat(ch4Emissions);
+          const ownerPercentageEmissions = selectedAsset.Owners.map(owner => ({
+            label: `${owner.CompanyName} - Percentage Emissions`,
+            value: ((parseFloat(owner.PercentageOfInterestCompany) / 100) * totalEmissions).toFixed(2)
+          }));
+          setOwnerEmissions(ownerPercentageEmissions);
+        } else {
+          // If no owners, set ownerEmissions to an empty array
+          setOwnerEmissions([]);
+        }
+    
+        // If the clicked feature is a country, fetch emissions data for that country and update selectedData
+        if (feature.properties.countryCode && feature.properties.countryName) {
+          const { countryCode, countryName } = feature.properties;
+    
+          // Fetch emissions data for the clicked country with a delay
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          const countryResponse = await fetch(`https://api.climatetrace.org/v4/country/emissions?&countries=${countryCode}`);
+          const countryEmissionsData = await countryResponse.json();
+    
+          // Extract CO2 emissions for the clicked country
+          const countryCo2Emissions = countryEmissionsData.length > 0 ? countryEmissionsData[0]?.emissions?.co2 || 'N/A' : 'N/A';
+    
+          // Update selectedData state with the fetched CO2 emissions data for the selected country
+          setSelectedData([
+            ...selectedData,
+            { label: `${countryName} - CO2 Emissions (Tons)`, value: countryCo2Emissions }
+          ]);
+        }
       }
     } catch (error) {
       console.error('Error fetching emissions data:', error);
