dc36c767bd452a75792a4a3bc2989616c784bfa3
Dean Hulkenberg <Dean.Hulkenberg@edu.sait.ca>
2024-03-21 13:02:55 -0700

    Create page.js
    Created Page.js for  news page

---
+++ app/components/News/page.js
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react';
+export default function News(){
+
+    const trendingcontainer = {
+
+    }
+
+    const trendingimgcontainer = {
+
+    }
+
+    const maincontainer = {
+
+    }
+
+    const textcontainer = {
+
+    }
+
+    const imgcontainer = {
+
+    }
+
+
+ /*    return(
+        
+        ); */
+ }
