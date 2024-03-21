515be7624fd2c7bb4d4d99d283473e4c4f6ed0f9
Dean Hulkenberg <Dean.Hulkenberg@edu.sait.ca>
2024-03-21 13:45:28 -0700

    Added notes
    added notes on what to add/ style

---
+++ app/components/News/page.js
@@ -0,0 +1,60 @@
+'use client';
+import React from 'react';
+export default function News(){
+
+    //trending tab container
+    const trendingcontainer = {
+
+    }
+
+    //contains individual trending items
+    const trendingitem = {
+
+    }
+
+    //size
+    //position
+    //border
+    const trendingimgcontainer = {
+
+    }
+
+    //size
+    //font
+    //color
+    //position
+    const trendingtextcontainer = {
+
+    }
+
+    //contains main items
+    const maincontainer = {
+
+    }
+
+    //contains text and img container
+    //contanins text container
+    const mainitem = {
+        
+    }
+    // color
+    // font
+    // text size
+    // position
+    // lines
+    // border
+    const textcontainer = {
+
+    }
+    //size
+    //location
+    //magin
+    //style
+    const imgcontainer = {
+
+    }
+
+    
+
+
+ }
