9358801d0256a42baebf611b17b3497f0530c181
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2024-03-22 13:15:26 -0600

    Map for CfCalc

--- app/CFCalc/MapRouting.js
+++ app/CFCalc/MapRouting.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
@@ -7,121 +7,130 @@ import L from 'leaflet';
 import 'leaflet-routing-machine';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import AirportData from './Airports.json'; // Import Airport data
 
 const MapRouting = () => {
-  const [sourceCity, setSourceCity] = useState(null);
-  const [destinationCity, setDestinationCity] = useState(null);
+  const [sourceAirport, setSourceAirport] = useState(null);
+  const [destinationAirport, setDestinationAirport] = useState(null);
   const [sourceOptions, setSourceOptions] = useState([]);
   const [destinationOptions, setDestinationOptions] = useState([]);
-  const provider = new OpenStreetMapProvider();
 
-  const handleSourceCityChange = async (_, value) => {
-    setSourceCity(value);
+  useEffect(() => {
+    // Populate source and destination options with airport data
+    setSourceOptions(AirportData);
+    setDestinationOptions(AirportData);
+  }, []);
+
+  const handleSourceAirportChange = (_, value) => {
+    setSourceAirport(value);
   };
 
-  const handleDestinationCityChange = async (_, value) => {
-    setDestinationCity(value);
+  const handleDestinationAirportChange = (_, value) => {
+    setDestinationAirport(value);
   };
 
-  const handleSearch = async (query, setOptions) => {
-    try {
-      const results = await provider.search({ query });
-      setOptions(results.map((result) => ({
-        label: result.label,
-        lat: result.y,
-        lng: result.x,
-      })));
-    } catch (error) {
-      console.error('Error searching:', error);
-      setOptions([]);
+  // Populate options with all airports when input is empty
+  const handleSearch = (query, setOptions) => {
+    if (!query) {
+      setOptions(AirportData);
+      return;
     }
+    // Filter options based on user input (query)
+    const filteredOptions = AirportData.filter(option =>
+      option.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setOptions(filteredOptions);
   };
 
   return (
     <div>
-      <MapContainer center={[20, 0]} zoom={6} >
-            <TileLayer 
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=97162abb-de48-4413-a227-44ce4cfb0cd3`}
-            />
-
+      <MapContainer center={[20, 0]} zoom={6}>
+        <TileLayer 
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=97162abb-de48-4413-a227-44ce4cfb0cd3`}
+        />
         <div className="flex items-end" style={{ position: 'absolute', top: '20px', right: '20px', zIndex: 1000 }}>
-            <div className='min-h-3/4 h-auto w-full bg-gray-400 flex justify-center items-center flex-wrap rounded-md backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
-                    <Autocomplete
-                        id='source-city'
-                        options={sourceOptions}
-                        onChange={handleSourceCityChange}
-                        getOptionLabel={(option) => option.label}
-                        onInputChange={(e, value) => handleSearch(value, setSourceOptions)}
-                        style={{ 
-                        width: 300, 
-                        background: 'rgba(0, 0, 0, 0.1)', // Black with 10% opacity
-                        borderRadius: '8px', // Rounded corners
-                        border: '1px solid #ccc', // Border
-                        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Box shadow
-                        color: '#fff', // Text color
-                        zIndex: 1000, // Ensure Autocomplete is above other elements
-                        }}
-                        renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            color='secondary'
-                            label='Source'
-                            variant='outlined'
-                            InputLabelProps={{ style: { color: 'purple' } }}
-                            style={{ background: 'transparent' }} // Set input field background to transparent
-                        />
-                        )}
-                    />
-                </div>
-                <div className='min-h-3/4 h-auto w-full bg-gray-400 flex justify-center items-center flex-wrap rounded-md backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
-                    <Autocomplete
-                    id='destination-city'
-                    options={destinationOptions}
-                    onChange={handleDestinationCityChange}
-                    getOptionLabel={(option) => option.label}
-                    onInputChange={(e, value) => handleSearch(value, setDestinationOptions)}
-                    style={{ 
-                        width: 300, 
-                        background: 'rgba(0, 0, 0, 0.1)', // Black with 10% opacity
-                        borderRadius: '8px', // Rounded corners
-                        border: '1px solid #ccc', // Border
-                        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Box shadow
-                        color: '#fff', // Text color
-                        zIndex: 1000, // Ensure Autocomplete is above other elements
-                    }}
-                    renderInput={(params) => (
-                        <TextField
-                        {...params}
-                        color='secondary'
-                        label='Destination'
-                        variant='outlined'
-                        InputLabelProps={{ style: { color: 'purple' } }}
-                        style={{ background: 'transparent' }} // Set input field background to transparent
-                        />
-                    )}
-                    />
-                </div>
-
-                {(sourceCity && destinationCity) && (
-          <Routing sourceCity={sourceCity} destinationCity={destinationCity} />
-        )}
+          <div className='min-h-3/4 h-auto w-full bg-gray-400 flex justify-center items-center flex-wrap rounded-md backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
+            <Autocomplete
+              id='source-airport'
+              options={sourceOptions}
+              onChange={handleSourceAirportChange}
+              getOptionLabel={(option) => option.name}
+              onInputChange={(e, value) => handleSearch(value, setSourceOptions)}
+              style={{ 
+                width: 300, 
+                background: 'rgba(0, 0, 0, 0.1)', // Black with 10% opacity
+                borderRadius: '8px', // Rounded corners
+                border: '1px solid #ccc', // Border
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Box shadow
+                color: '#fff', // Text color
+                zIndex: 1000, // Ensure Autocomplete is above other elements
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  color='secondary'
+                  label='Source'
+                  variant='outlined'
+                  InputLabelProps={{ style: { color: 'purple' } }}
+                  style={{ background: 'transparent' }} // Set input field background to transparent
+                />
+              )}
+            />
+          </div>
+          <div className='min-h-3/4 h-auto w-full bg-gray-400 flex justify-center items-center flex-wrap rounded-md backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
+            <Autocomplete
+              id='destination-airport'
+              options={destinationOptions}
+              onChange={handleDestinationAirportChange}
+              getOptionLabel={(option) => option.name}
+              onInputChange={(e, value) => handleSearch(value, setDestinationOptions)}
+              style={{ 
+                width: 300, 
+                background: 'rgba(0, 0, 0, 0.1)', // Black with 10% opacity
+                borderRadius: '8px', // Rounded corners
+                border: '1px solid #ccc', // Border
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Box shadow
+                color: '#fff', // Text color
+                zIndex: 1000, // Ensure Autocomplete is above other elements
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  color='secondary'
+                  label='Destination'
+                  variant='outlined'
+                  InputLabelProps={{ style: { color: 'purple' } }}
+                  style={{ background: 'transparent' }} // Set input field background to transparent
+                />
+              )}
+            />
+          </div>
         </div>
+        {(sourceAirport && destinationAirport) && (
+          <Routing sourceAirport={sourceAirport} destinationAirport={destinationAirport} />
+        )}
       </MapContainer>
     </div>
   );
 };
 
-const Routing = ({ sourceCity, destinationCity }) => {
+const Routing = ({ sourceAirport, destinationAirport }) => {
   const map = useMap();
+  
 
-  L.Routing.control({
-    waypoints: [
-      L.latLng(parseFloat(sourceCity.lat), parseFloat(sourceCity.lng)),
-      L.latLng(parseFloat(destinationCity.lat), parseFloat(destinationCity.lng)),
-    ],
-  }).addTo(map);
+  useEffect(() => {
+    if (sourceAirport && destinationAirport) {
+      const waypoints = [
+        L.latLng(parseFloat(sourceAirport.latitude_deg), parseFloat(sourceAirport.longitude_deg)),
+        L.latLng(parseFloat(destinationAirport.latitude_deg), parseFloat(destinationAirport.longitude_deg)),
+      ];
+
+      L.Routing.control({
+        waypoints,
+      }).addTo(map);
+    }
+  }, [sourceAirport, destinationAirport, map]);
 
   return null;
 };
