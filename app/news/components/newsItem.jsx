6740e400fa5b68c5dcd02c6aa2506ca64d88cea1
aaaarvieeee <119615693+aaaarvieeee@users.noreply.github.com>
2024-03-24 18:58:33 -0600

    basic news page

---
+++ app/news/components/newsItem.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+export default function newsItem() {
+    
+
+
+    return(
+    <div className='flex flex-row'>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+        <h1>News Item</h1>
+
+    </div>
+    )
+}
