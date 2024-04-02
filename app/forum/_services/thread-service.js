1c8f015e1ee107198816b92bb7194d8357125c2d
Rianne <r.d.hempsall@gmail.com>
2024-04-02 13:05:42 -0600

    Decoupled threads from users into it's own collection in database.

--- app/forum/_services/thread-service.js
+++ app/forum/_services/thread-service.js
@@ -1,13 +1,16 @@
 import { Threads } from "openai/resources/beta/threads/threads";
 import { db } from "../../_utils/firebase";
-import { collection, getDocs, addDoc, query } from "firebase/firestore";
+import { collection, getDocs, addDoc, query, doc, remove } from "firebase/firestore";
 
 export const addThread = async(userId, thread) => {
     try{
-        const itemCol = collection(db, `users/${userId}/threads`);
+        if (userId !== null) {
+            console.log("Inside add threads");
+        const itemCol = collection(db, `threads`);
         const docRef = await addDoc(itemCol, thread);
         // thread.threadId = docRef;
-        return docRef.id;
+        return docRef.id; 
+        }
     }
     catch(error){
         console.log(error);
@@ -18,25 +21,18 @@ export const addThread = async(userId, thread) => {
 
 export const getThreads = async(userId) => {
     try{
-        const itemCol = collection(db, `users/${userId}/threads`);
+        if (userId !== null) {
+            console.log("Inside get threads");
+        const itemCol = collection(db, `threads`);
         const querySnapshot = await getDocs(itemCol);
         const threads = [];
         querySnapshot.forEach((doc) => {
             threads.push({id: doc.id, ...doc.data()});
         });
         return threads;
+        }
     }
     catch(error){
-        console.log(error);
-    }
-}
-
-export const editThread = async(userID) => {
-    try {
-        const itemCol = collection(db, `users/${userId}/threads`);
-        const querySnapshot = await getDocs(itemcol, );
-    }
-    catch (error){
         console.log(error);
     }
 }
--- app/forum/page.js
+++ app/forum/page.js
@@ -1,12 +1,14 @@
 "use client"
 import React, { useState, useEffect } from "react";
 import { UserAuth } from "../context/AuthContext.js";
-import { addThread, getThreads } from "./_services/thread-service.js";
+import { addThread, getThreads} from "./_services/thread-service.js";
+import { Button } from "@mui/material";
 // CreatedAT (DATE), POST ID, Title, Content, Edit, Reply
 // EDIT ? Firebase
 
 export default function ForumPage () {
     const [threads, setThreads] = useState([]);
+    const [replies, setReplies] = useState([]);
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [date, setDate] = useState("");
@@ -14,17 +16,25 @@ export default function ForumPage () {
     const thread = {
         title: title,
         content: content,
+        author: "",
         date: date,
-        threadId: ""
+        threadUid: "",
+        replies: replies
     };
 
     // Write a getDate that formats the date object for a string. Potentially delete and re-create DB for string date.
     const { user } = UserAuth(); // Get the user from the auth hook
-    const handleAddThread = (newThread) => {  
-
-        newThread.threadId = addThread(user.uid, newThread);
+    const handleAddThread = (newThread) => { 
+        if(user.displayName === null) {
+            newThread.author = "Anonymous"
+        }
+        else {
+            newThread.author = user.displayName
+        }
+        newThread.threadUid = user.uid;
+        addThread(user.uid, newThread);
         setThreads([...threads, newThread]);  
-    } 
+    };
     //GET threads and populate page.
     const loadThreads = async () => {
             const storedThreads = await getThreads(user.uid);
@@ -62,9 +72,8 @@ export default function ForumPage () {
             handleAddThread(thread); 
             setTitle("");
             setContent("");
-            // setDate("");
         } catch (error) {
-            console.error('Error submitting report:', error);
+            console.error('Error submitting thread:', error);
             // Handle error
         }
     }
@@ -72,12 +81,11 @@ export default function ForumPage () {
     //DELETE the thread.
     //Refresh page and remove info - delete from db itself.
     //UPDATE the thread. 
-    //Update front page component when user makes a thread OR database is upda
+
 
     useEffect(() => {
         if (user) {
-            loadThreads();
-            
+            loadThreads(); 
         }
         else{
             return;
@@ -123,20 +131,29 @@ export default function ForumPage () {
                     className='py-4 px-6 text-white'  
                     onSubmit={addThread} type="submit">CREATE THREAD</button>
                     </form>
-                    </div>
-                
-            <div className="threadsContainer">
-                {/* Display threads here */}
+            <div className="threadsContainer m-2 py-2 ">
+                {/* Display threads here  */}
                 {threads.map((thread) => (
-                    <div key={thread.id} className="p-3 bg-gray-800 rounded-md m-3 text-white">
+                    <div key={thread.id} className="p-3 bg-gray-500 rounded-md m-3 text-white">
                         <h2>{thread.title}</h2>
                         <p>{thread.content}</p>
-                        
-                        {/* <p>{thread.date}</p> */}
+                        {console.log(thread.threadUid)}
+                        <div>
+                        <Button className=' m-2 text-decoration-none btn btn-sm btn-success flex-row'
+                        // onSubmit={handleEdit}
+                        >Edit</Button>
+                        </div> 
+                        <div>
+                        <Button className='m-2 text-decoration-none btn btn-sm btn-success flex-row'
+                        // onClick={ () => deleteThread(/delete/${thread.id})}
+                        >Delete</Button>
+                        </div>
+                        <p>{thread.date}</p>
                     </div>
                 ))}
+                </div>
             </div>
-            </main>
-        </>
+        </main>
+    </>
     );
 };
