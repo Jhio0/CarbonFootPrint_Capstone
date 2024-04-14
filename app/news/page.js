de78bdad41d9bea2100f26649009d8733f909053
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2024-04-14 10:55:54 -0600

    Fix dean part

--- app/news/page.js
+++ app/news/page.js
@@ -2,16 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import newsBar from "./components/newsBar";
-
-// use this api to get the news
-
-// https://rapidapi.com/cvera08/api/climate-crisis
-
 
 export default function NewsItem({ news }) {
     const [newsList, setNewsList] = useState([]);
-    const newsListUtil = [...newsList]
 
     const url = 'https://climate-news-feed.p.rapidapi.com/page/1?limit=10';
     const options = {
@@ -36,23 +29,21 @@ export default function NewsItem({ news }) {
     useEffect(() => {
         fetchNews();
     }, []);
-    
 
     return (
-        <div className="bg-color-[#1E1E1C] ">
+        <div className="bg-color-[#1E1E1C]">
             <h1 className="bg-black text-center text-white font-bold text-2xl pt-3 mx-10 font-serif">News</h1>
-            <div className="flex flex-wrap place-content-center bg-black mx-10 pb-10 ">
-                {newsListUtil.map((newsItem) => (
+            <div className="flex flex-wrap place-content-center bg-black mx-10 pb-10">
+                {newsList.map((newsItem) => (
                     <div key={newsItem.id} className="news-container w-2/5 border-4 bg-ferngreen-700 p-4 m-4 border-b-ferngreen-900 border-r-ferngreen-900  border-t-ferngreen-600 border-l-ferngreen-600">
                         <h2 className="text-xl font-bold text-white font-Typography">{newsItem.title}</h2>
                         <p className="text-white font-Typography">{newsItem.description}</p>
-                        <Link className="text-white underline " href={`${newsItem.url}`}>
+                        <Link className="text-white underline" href={`${newsItem.url}`}>
                             Read More
                         </Link>
                     </div>
                 ))}
             </div>
-           
         </div>
     );
 }
